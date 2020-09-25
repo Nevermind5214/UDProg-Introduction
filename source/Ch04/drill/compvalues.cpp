61160452575b344dfbec20e0d1cb06a54a6c9187
David Mikaczo <nevermind5214@gmail.com>
2020-09-25 20:01:05 +0200

    Fiexs

--- source/Ch04/drill/compvalues.cpp
+++ source/Ch04/drill/compvalues.cpp
@@ -12,7 +12,7 @@ int main()
         if(a > b) cout << "A is bigger.\n\n";
         else if(a < b) cout << "B is bigger\n\n";
         else cout << "the numbers are equal\n\n";
-        if(abs(a-b)<0.01) cout << "the numbers are almost equal\n";
+        if(abs(a-b) < 0.01) cout << "the numbers are almost equal\n";
     }
 
     return 0;
--- source/Ch04/drill/compvalues6tol.cpp
+++ source/Ch04/drill/compvalues6tol.cpp
@@ -2,10 +2,10 @@
 
 double to_meters(double value, string unit)
 {
-    if(unit == "cm") value=value/100;
+    if(unit == "cm") value = value / 100;
     else if(unit == "m");
-    else if(unit == "in") value=value*2.54/100;
-    else if(unit == "ft") value=value*2.54*12/100;
+    else if(unit == "in") value = value * 2.54 / 100;
+    else if(unit == "ft") value = value * 2.54 * 12/100;
     else error("Illegal unit, now the POPO is coming for u");
     return value;
 }
@@ -13,8 +13,12 @@ double to_meters(double value, string unit)
 int main()
 {
     double a = 0, sum = 0;
-    int smallest = 0, largest=0;
-    string unit="";
+    int smallest = 0, largest = 0;
+
+    string unit = "";
+    string sm_unit = "";
+    string lg_unit = "";
+
     vector<double> values;
     vector<double> values_in_meters;
 
@@ -34,22 +38,24 @@ int main()
 
     	sum += converted;
 
-        if(converted > values_in_meters[largest])
+        if(converted > values_in_meters[largest] || lg_unit == "")
         {
         	largest = values.size() - 1;
+        	lg_unit = unit;
         	cout << "The largest so far\n";
         }
-        if(converted < values_in_meters[smallest])
+        if(converted < values_in_meters[smallest] || sm_unit == "")
         {
         	smallest = values.size() - 1;
+        	sm_unit = unit;
         	cout << "The smallest so far\n";
         }
 	}
 
-    cout << "-----\nThe smallest: " << values[smallest] << endl;
-    cout << "The largest: " << values[largest] << endl;
+    cout << "\n-----\nThe smallest: " << values[smallest] << sm_unit << endl;
+    cout << "The largest: " << values[largest] << lg_unit << endl;
     cout << "The number of values: " << values.size() << endl;
-    cout << "The sum of the values: " << sum << endl;
+    cout << "The sum of the values: " << sum << " m\n";
 
     sort(values_in_meters);
     for(int i=0;i<values_in_meters.size();i++) cout << values_in_meters[i] << " m\n";

6c012b6bc67412765789250df9f1bcd42052a0d8
Nevermind5214 <nevermind5214@gmail.com>
2020-11-03 20:31:07 +0100

    Update calculator08fixed.cpp
    iz shit pass pls

--- source/Ch07/drill/calculator08fixed.cpp
+++ source/Ch07/drill/calculator08fixed.cpp
@@ -4,35 +4,43 @@ class Token {
 public:
 	char kind;	// what kind of token
 	double value;	// for numbers: a value 
-	string name;
+	string name;	//name for user defined tokens
+
 	Token(char ch) :kind(ch), value(0) { } // make a Token from a char
 	Token(char ch, double val) :kind(ch), value(val) { } // make a Token from a char and a double
 	Token(char ch, string n) :kind(ch), name(n) { } // make a Token from a char and a double
 };
 
 class Token_stream {
-	bool full;
-	Token buffer;
-public:
-	Token_stream() :full(0), buffer(0) { }
+public:											// make a Token_stream that reads from cin
+	Token_stream() :full(false), buffer(0) { }	// The constructor just sets full to indicate that the buffer is empty
 
-	Token get();
-	void unget(Token t) { buffer = t; full = true; }
+	Token get();	// get a Token (get() is defined elsewhere)
+	void unget(Token t) { buffer = t; full = true; } // put a Token back
+	// The unget() member function puts its argument back into the Token_stream's buffer:
 
 	void ignore(char);
+private:
+	bool full;
+	Token buffer;
 };
 
-const char let = 'L';
+// Token kinds
+const char let = '#';
 const char quit = 'Q';
 const char print = ';';
 const char number = '8';
 const char name = 'a';
+const char mysqrt = 's';
+const char mypow = 'p';
+
 
 Token Token_stream::get()
 {
 	if (full) { full = false; return buffer; }	//do we already have a Token ready?
+												//remove token from buffer
 	char ch;
-	cin >> ch;
+	cin >> ch;  // note that >> skips whitespace (space, newline, tab, etc.)
 	switch (ch) {
 		case '(':
 		case ')':
@@ -41,8 +49,8 @@ Token Token_stream::get()
 		case '*':
 		case '/':
 		case '%':
-		case ';':
-		case '=':
+		case ';':	// for "print"
+		case '=':	// for the variables
 		return Token(ch);
 		case '.':
 		case '0':
@@ -55,196 +63,218 @@ Token Token_stream::get()
 		case '7':
 		case '8':
 		case '9':
-		{	cin.unget();
+		{	cin.unget();	// put digit back into the input stream
 			double val;
-			cin >> val;
-			return Token(number, val);
+			cin >> val;	// read a floating-point number
+			return Token(number, val);	// const char number = '8'
 		}
 		default:
-		if (isalpha(ch)) {
+		if (isalpha(ch)) { // if it's a char but not one of the above
 			string s;
 			s += ch;
 			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
-			cin.unget();
-			if (s == "let") return Token(let);
-			if (s == "quit") return Token(quit);
+			cin.unget(); // put digit back into the input stream
+			if (s == "let") return Token(let); // make a new token
+			if (s == "quit") return Token(quit); // this is where we quit
+			if (s == "mysqrt") return Token(mysqrt); // square root
+			if (s == "mypow") return Token(mypow); // power
 			return Token(name, s);
 		}
 		error("Bad token");
+		return 0;	//Return for: warning: control reaches end of non-void function
 	}
 }
 
+// ignore until c
 void Token_stream::ignore(char c)
 {
 	if (full && c == buffer.kind) {
 		full = false;
 		return;
 	}
-	full = false;
+	full = false; // ignore token
 
 	char ch;
 	while (cin >> ch)
 		if (ch == c) return;
 }
 
-struct Variable {
+class Variable {
+public:
 	string name;
 	double value;
-	Variable(string n, double v) :name(n), value(v) { }
 };
 
 vector<Variable> names;
 
-double get_value(string s)
+double get_value(string s)  // returns the value of a variable
 {
 	for (int i = 0; i < names.size(); ++i)
 		if (names[i].name == s) return names[i].value;
 	error("get: undefined name ", s);
+	return 0;	//Return for: warning: control reaches end of non-void function
 }
 
-void set_value(string s, double d)
+void set_value(string s, double d)  // sets the value of a user defined variabla
 {
 	for (int i = 0; i <= names.size(); ++i)
+	{
 		if (names[i].name == s) {
 			names[i].value = d;
 			return;
 		}
 		error("set: undefined name ", s);
 	}
+}
 
-	bool is_declared(string s)
-	{
-		for (int i = 0; i < names.size(); ++i)
-			if (names[i].name == s) return true;
-		return false;
-	}
+bool is_declared(string s)  // checks whether or not a user defined
+							// variable is already declared or not
+{
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return true;
+	return false;
+}
 
-	Token_stream ts;
+Token_stream ts;
 
-	double expression();
+double expression();  // defined elsewhere
 
-	double primary()
-	{
-		Token t = ts.get();
-		switch (t.kind) {
-			case '(':
-			{	double d = expression();
-				t = ts.get();
-				if (t.kind != ')') error("'(' expected");
-			}
-			case '-':
-			return -primary();
-			case number:
-			return t.value;
-			case name:
-			return get_value(t.name);
-			default:
-			error("primary expected");
+// deal with numbers, negative numbers and parentheses
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+		case '(':   // handle '(' expression ')'
+		{	double d = expression();
+			t = ts.get();
+			if (t.kind != ')') error("'(' expected");
 		}
-	}
-
-	double term()
-	{
-		double left = primary();
-		while (true) {
-			Token t = ts.get();
-			switch (t.kind) {
-				case '*':
-				left *= primary();
-				break;
-				case '/':
-				{	double d = primary();
-					if (d == 0) error("divide by zero");
-					left /= d;
-					break;
-				}
-				default:
-				ts.unget(t);
-				return left;
-			}
-		}
-	}
-
-	double expression()
-	{
-		double left = term();
-		while (true) {
-			Token t = ts.get();
-			switch (t.kind) {
-				case '+':
-				left += term();
-				break;
-				case '-':
-				left -= term();
-				break;
-				default:
-				ts.unget(t);
-				return left;
-			}
-		}
-	}
-
-	double declaration()
-	{
-		Token t = ts.get();
-		if (t.kind != 'a') error("name expected in declaration");
-		string name = t.name;
-		if (is_declared(name)) error(name, " declared twice");
-		Token t2 = ts.get();
-		if (t2.kind != '=') error("= missing in declaration of ", name);
-		double d = expression();
-		names.push_back(Variable(name, d));
-		return d;
-	}
-
-	double statement()
-	{
-		Token t = ts.get();
-		switch (t.kind) {
-			case let:
-			return declaration();
-			default:
-			ts.unget(t);
-			return expression();
-		}
-	}
-
-	void clean_up_mess()
-	{
-		ts.ignore(print);
-	}
-
-	const string prompt = "> ";
-	const string result = "= ";
-
-	void calculate()
-	{
-		while (true) try {
-			cout << prompt;
-			Token t = ts.get();
-			while (t.kind == print) t = ts.get();
-			if (t.kind == quit) return;
-			ts.unget(t);
-			cout << result << statement() << endl;
-		}
-		catch (runtime_error& e) {
-			cerr << e.what() << endl;
-			clean_up_mess();
-		}
-	}
-
-	int main()
-
-	try {
-		calculate();
+		case '-':     // deal with negative numbers
+		return -primary();
+		case number:	// deal with normal numbers
+		return t.value;
+		case name:	// deal with  variables
+		return get_value(t.name);
+		case mysqrt:
 		return 0;
+		case mypow:
+		return 0;
+		default:
+		error("primary expected");
+		return 0;   //Return for: warning: control reaches end of non-void function
 	}
-	catch (exception& e) {
-        cerr << "error: " << e.what() << endl;
-        return 1;
-    }
+}
 
-    catch (...) {
-        cerr << "Oops: unknown exception!\n";
-        return 2;
-    }
+// deal with *, /, and %
+double term()
+{
+	double left = primary();
+	while (true) {
+		Token t = ts.get();
+		switch (t.kind) {
+			case '*':
+			left *= primary();
+			break;
+			case '/':
+			{	double d = primary();
+				if (d == 0) error("divide by zero");
+				left /= d;
+				break;
+			}
+        	case '%':
+        	{
+            	left = fmod(left, primary());
+            	t = ts.get();
+            	break;
+            }
+			default:
+			ts.unget(t);	// put t back into the token stream
+			return left;
+		}
+	}
+}
+
+// deal with + and -
+double expression()
+{
+	double left = term();	// read and evaluate a Term
+	while (true) {
+		Token t = ts.get();	// get the next token from token stream
+		switch (t.kind) {
+			case '+':
+			left += term();	// evaluate Term and add
+			break;
+			case '-':
+			left -= term();	// evaluate Term and subtract
+			break;
+			default:
+			ts.unget(t);	// put t back into the token stream
+			return left;	// finally: no more + or -: return the answer
+		}
+	}
+}
+
+double declaration()  // this happens when the user enters let
+{
+	Token t = ts.get(); // get the next token from token stream
+	if (t.kind != 'a') error("name expected in declaration");  //If its not a name
+	string name = t.name;
+	if (is_declared(name)) error(name, " declared twice");
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of ", name);
+	double d = expression();
+	names.push_back(Variable{name, d});  // we add our new variable to the Variables vector
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+		case let:
+		return declaration();
+		default:
+		ts.unget(t);
+		return expression();
+	}
+}
+
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
+
+const string prompt = "> ";
+const string result = "= ";
+
+void calculate()
+{
+	while (true) try {
+		cout << prompt;
+		Token t = ts.get();
+		while (t.kind == print) t = ts.get(); // ';' for "print now"
+		if (t.kind == quit) return;	// 'x' for quit
+		ts.unget(t);
+		cout << result << statement() << endl;
+	}
+	catch (runtime_error& e) {
+		cerr << e.what() << endl;
+		clean_up_mess();
+	}
+}
+
+int main()
+
+try {
+	calculate();	//we moved our while loop outside of the main (idk why tho)
+	return 0;
+}
+catch (exception& e) {
+	cerr << "error: " << e.what() << endl;
+	return 1;
+}
+
+catch (...) {
+	cerr << "Oops: unknown exception!\n";
+	return 2;
+}
